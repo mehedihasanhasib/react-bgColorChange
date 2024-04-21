c0a9762008da6240b175ef1d91a154839b044b62
Mehedi Hasan Hasib <mehedi.mh50@gmail.com>
2024-04-22 01:17:55 +0600

    completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,20 @@
 import { useState } from 'react';
-import Button from './components/Button';
 
 function App() {
-  const [color, setColor] = useState("");
+  const [color, setColor] = useState("black");
+
+  const btnColors = ["red", "blue", "yellow", "green", "orange", "tomato", "grey"];
+
+  const btns = btnColors.map((element, index) => {
+    return (<button style={{ backgroundColor: element }} className='p-3 rounded-full' key={index} onClick={() => setColor(element)} ></button >);
+  });
 
   return (
     <>
-      <div
-        className="w-full h-screen flex justify-center"
-        style={{ backgroundColor: color }}
-      >
-        hello
-
-        <Button bg={color} />
-
-        {/* <button className='bg-red-500 fixed bottom-5'>
-          Button
-        </button> */}
+      <div className='w-full h-screen flex justify-center duration-200 ' style={{ backgroundColor: color }}>
+        <div className='fixed bottom-6 flex gap-1 bg-white p-2 rounded-xl '>
+          {btns}
+        </div>
       </div>
     </>
   );
